3b0a9b5b3c75da21d6c3aa52395a64b2015ed65a
DreschGH <gabriel.dresch.sc@gmail.com>
2025-11-25 14:05:48 -0300

    feat: sending init

--- Datalab/__init__.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from typing import List, Optional
-
-# Set plot style for better aesthetics
-sns.set_theme(style="whitegrid")
-plt.rcParams['figure.figsize'] = (12, 6)
