09f3cb76f75e6e69946315a8b7e449d412c73cfc
DreschGH <gabriel.dresch.sc@gmail.com>
2025-11-25 14:03:00 -0300

    feat: inital stages

--- Datalab/datalab.py
+++ Datalab/datalab.py
@@ -3,11 +3,20 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from typing import List, Optional
+import logging
 
 #--> plot style
 sns.set_theme(style="whitegrid")
 plt.rcParams['figure.figsize'] = (12, 6)
 
 
-class DataLab:
-    pass
+class DataLab:   #<-- Main Class
+    '''
+    Class that performs data analisys, from overall eval, to health checking and missing values treatments. 
+    Each function must have their docstrings available, if you need any extra details
+    '''
+    def __init__(self, df: pd.DataFrame, df_name: str = "Dataset name"):
+        self.df = df
+        self.df_name = df_name
+        
+        pass
