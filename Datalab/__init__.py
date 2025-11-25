602723009061eaa2dc5b05af52a019cb33360646
DreschGH <gabriel.dresch.sc@gmail.com>
2025-11-25 13:56:45 -0300

    feat: starting development

---
+++ Datalab/__init__.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from typing import List, Optional
+
+# Set plot style for better aesthetics
+sns.set_theme(style="whitegrid")
+plt.rcParams['figure.figsize'] = (12, 6)
---
+++ Datalab/datalab.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from typing import List, Optional
+
+#--> plot style
+sns.set_theme(style="whitegrid")
+plt.rcParams['figure.figsize'] = (12, 6)
+
+
+class DataLab:
+    pass
